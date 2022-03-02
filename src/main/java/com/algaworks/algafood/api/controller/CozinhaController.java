26e535faa6e52aae291ec5d355143eb75b92dbf4
Mucumbe <mucumbeblandino@gmail.com>
2022-03-03 00:19:48 +0200

    Modelando e implementando a atualização de recursos com PUT

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,13 +10,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
+
+import ch.qos.logback.core.joran.util.beans.BeanUtil;
 
 @RestController
 @RequestMapping("/cozinhas")
@@ -38,11 +43,24 @@ public class CozinhaController {
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
-		
 		return cozinhaRepository.adicionar(cozinha);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> actualizar(@PathVariable Long id,@RequestBody Cozinha cozinha) {
+		Cozinha cozinhaactual= cozinhaRepository.buscar(id);
+		if (cozinhaactual!=null) {
+
+		
+		 BeanUtils.copyProperties(cozinha, cozinhaactual,"id");
+		 cozinhaactual= cozinhaRepository.adicionar(cozinhaactual);
+		 return ResponseEntity.ok(cozinhaactual);
+	}
+		return ResponseEntity.notFound().build();
+		
 	}
 }
