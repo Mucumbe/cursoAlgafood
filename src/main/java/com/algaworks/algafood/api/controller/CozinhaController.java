683e9cabea81b7447b656c64b211f816117b9b1e
Mucumbe <mucumbeblandino@gmail.com>
2022-03-02 23:49:11 +0200

    Modelando e implementando a inclusão de recursos com POST

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -8,7 +8,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -20,21 +23,26 @@ public class CozinhaController {
 
 	@Autowired
 	CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping
-	public List<Cozinha> listar(){
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
-		Cozinha cozinha= cozinhaRepository.buscar(id);
-		
-		if (cozinha !=null) {
+		Cozinha cozinha = cozinhaRepository.buscar(id);
+
+		if (cozinha != null) {
 			return ResponseEntity.ok(cozinha);
 		}
 		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		
+		return cozinhaRepository.adicionar(cozinha);
 	}
 }
