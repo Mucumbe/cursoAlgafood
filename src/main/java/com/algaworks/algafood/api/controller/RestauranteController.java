8d5a0577d2db4509b87c17d6048984eefe4c76b3
Mucumbe <mucumbeblandino@gmail.com>
2022-03-07 13:16:23 +0200

    Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -3,14 +3,20 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
 @RequestMapping("/restaurantes")
@@ -18,6 +24,9 @@ public class RestauranteController {
 
 	@Autowired
 	RestauranteRepository repository;
+
+	@Autowired
+	private CadastroRestauranteService service;
 
 	@GetMapping
 	public List<Restaurante> listar() {
@@ -33,5 +42,32 @@ public class RestauranteController {
 		}
 		return ResponseEntity.notFound().build();
 
+	}
+
+	@PostMapping
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
+		try {
+			restaurante = service.salvar(restaurante);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<?> actualizar(@RequestBody Restaurante restaurante,@PathVariable long id){
+		Restaurante restauranteactual=repository.buscar(id);
+		if (restauranteactual==null) {
+			return ResponseEntity.notFound().build();
+		}else {
+			try {
+				restaurante=service.actualizar(restaurante, id);
+				return ResponseEntity.ok(restaurante);
+			} catch (EntidadeNaoEncontradaException e) {
+				return ResponseEntity.badRequest().body(e.getMessage());
+			}
+		}
+		
+		
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.model.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+
+@Service
+public class CadastroRestauranteService {
+
+	@Autowired
+	RestauranteRepository repository;
+	@Autowired
+	CozinhaRepository cozinhaRepository;
+
+	public Restaurante salvar(Restaurante restaurante) {
+		long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com Codigo: %d", cozinhaId));
+		}
+		restaurante.setCozinha(cozinha);
+		restaurante = repository.adicionar(restaurante);
+		return restaurante;
+	}
+
+	public Restaurante actualizar(Restaurante restaurante,long restauranteId) {
+		
+		long cozinhaId=restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.buscar(cozinhaId);
+		
+		
+		if (cozinha == null) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não existe cadastro de cozinha com Codigo: %d", cozinhaId));
+		}
+		restaurante.setCozinha(cozinha);
+		Restaurante restauranteActual = repository.buscar(restauranteId);
+		BeanUtils.copyProperties(restaurante, restauranteActual, "id");
+		restauranteActual = repository.adicionar(restauranteActual);
+		return restauranteActual;
+	}
+}
